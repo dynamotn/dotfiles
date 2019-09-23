3a9b0fe5d541f9ac3d27ce62d8a3b6bfe74619ca
Trần Đức Nam <dynamo.foss@gmail.com>
2019-09-23 10:09:41 +0700

    Update setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 
 SETUP_DIR=$(dirname "$(readlink -f "$0")")
 
+_usage() {
+  echo "$0 usage:" && grep " .)\\ #" $0 | sed -e 's/\(.\)) #/-\1/g'
+  exit 0;
+}
+
 _link() {
   if [[ -e "$2" || -L "$2" ]]; then
     if test "$1" -ef "$(readlink -f "$2")"; then
@@ -9,6 +14,10 @@ _link() {
       return
     else
       rm -rf "$2"
+    fi
+  else
+    if [[ -d $1 && $HOME != "$(dirname "$2")" ]]; then
+      rm -rf "$(dirname "$2")"
     fi
   fi
   mkdir -p "$(dirname "$2")"
@@ -31,5 +40,15 @@ _install() {
   _link "$SETUP_DIR/bat" ~/.config/bat
 }
 
-git --work-tree=$SETUP_DIR submodule update --init --recursive --remote
+while getopts "hs" arg; do
+  case $arg in
+    s) # Update git submodule before setup
+      git --work-tree=$SETUP_DIR submodule update --init --recursive --remote
+      ;;
+    h) # Display help
+      _usage
+      ;;
+  esac
+done
+
 _install
