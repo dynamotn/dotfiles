1372138dd9d33e79cd3c5c4eaade057a6642fd25
Trần Đức Nam <dynamo.foss@gmail.com>
2023-02-22 02:27:46 +0700

    feat: install ssh config before other

--- setup.sh
+++ setup.sh
@@ -20,22 +20,22 @@ _install_chezmoi() {
 
 _install_age() {
   if [ ! "$(command -v age)" ]; then
-    local temp=$(mktemp)
+    temp=$(mktemp)
     if [ "$(command -v curl)" ]; then
-      curl -sSL -o $temp https://dl.filippo.io/age/latest?for=linux/amd64
+      curl -sSL -o "$temp" https://dl.filippo.io/age/latest?for=linux/amd64
     elif [ "$(command -v wget)" ]; then
-      wget -qO $temp https://dl.filippo.io/age/latest?for=linux/amd64
+      wget -qO "$temp" https://dl.filippo.io/age/latest?for=linux/amd64
     else
       echo "To install age, you must have curl or wget installed." >&2
       exit 1
     fi
 
     if [ "$(command -v tar)" ]; then
-      tar -C $BIN_DIR -xzf $temp --strip=1 age/age
+      tar -C "$BIN_DIR" -xzf "$temp" --strip=1 age/age
     else
       echo "To install age, you must have tar and gzip installed." >&2
     fi
-    rm -rf $temp
+    rm -rf "$temp"
   fi
 }
 
@@ -47,17 +47,22 @@ _main() {
   _install_age
 
   # Run chezmoi in debug mode
-  local chezmoi_params=""
+  chezmoi_params=""
   if [ "$DEBUG" = "true" ]; then
     chezmoi_params="$chezmoi_params --debug"
   fi
 
   # Modify source directory of chezmoi, manipulate chezmoi config and generate
   # to chezmoi's default config template
-  echo "sourceDir = \"$SETUP_DIR\"" > $SETUP_DIR/home/.chezmoi.toml.tmpl
-  cat $SETUP_DIR/.chezmoi.toml.tmpl >> $SETUP_DIR/home/.chezmoi.toml.tmpl
-  chezmoi init $chezmoi_params -S "$SETUP_DIR" \
-    && chezmoi apply $chezmoi_params
+  echo "sourceDir = \"$SETUP_DIR\"" > "$SETUP_DIR"/home/.chezmoi.toml.tmpl
+  cat "$SETUP_DIR"/.chezmoi.toml.tmpl >> "$SETUP_DIR"/home/.chezmoi.toml.tmpl
+  chezmoi init "$chezmoi_params" -S "$SETUP_DIR"
+
+  # Apply configuration by order
+  # shellcheck disable=SC2086
+  chezmoi apply $chezmoi_params "$HOME/.ssh"
+  # shellcheck disable=SC2086
+  chezmoi apply $chezmoi_params
 }
 
 if [ "$DEBUG" = "true" ]; then
