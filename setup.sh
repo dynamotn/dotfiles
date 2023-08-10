2b8fd50b08aca4feb521c79b5f08bb4b9f57bfce
Trần Đức Nam <dynamo.foss@gmail.com>
2023-08-11 02:16:24 +0700

    refactor: more verbose and trap in scripts

--- setup.sh
+++ setup.sh
@@ -1,19 +1,21 @@
-#!/bin/sh
-set -eou pipefail
+#!/bin/bash
+set -Eeou pipefail
 
 SETUP_DIR=$(dirname "$(readlink -f "$0")")
+source "$SETUP_DIR/scripts/message.sh"
+
 BIN_DIR="$HOME/.local/bin"
 DEBUG="${1:-false}"
 
 _install_chezmoi() {
   if [ ! "$(command -v chezmoi)" ]; then
+    _notice "Install chezmoi"
     if [ "$(command -v curl)" ]; then
       sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$BIN_DIR"
     elif [ "$(command -v wget)" ]; then
       sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$BIN_DIR"
     else
-      echo "To install chezmoi, you must have curl or wget installed." >&2
-      exit 1
+      _error "To install chezmoi, you must have curl or wget installed."
     fi
   fi
 }
@@ -26,14 +28,13 @@ _install_age() {
     elif [ "$(command -v wget)" ]; then
       wget -qO "$temp" https://dl.filippo.io/age/latest?for=linux/amd64
     else
-      echo "To install age, you must have curl or wget installed." >&2
-      exit 1
+      _error "To install age, you must have curl or wget installed."
     fi
 
     if [ "$(command -v tar)" ]; then
       tar -C "$BIN_DIR" -xzf "$temp" --strip=1 age/age
     else
-      echo "To install age, you must have tar and gzip installed." >&2
+      _error "To install age, you must have tar and gzip installed."
     fi
     rm -rf "$temp"
   fi
@@ -54,13 +55,17 @@ _main() {
 
   # Modify source directory of chezmoi, manipulate chezmoi config and generate
   # to chezmoi's default config template
+  _notice "Initialize chezmoi"
   echo "sourceDir = \"$SETUP_DIR\"" > "$SETUP_DIR"/home/.chezmoi.toml.tmpl
   cat "$SETUP_DIR"/.chezmoi.toml.tmpl >> "$SETUP_DIR"/home/.chezmoi.toml.tmpl
+  _notice "Please answer the following questions"
   chezmoi init "$chezmoi_params" -S "$SETUP_DIR" --prompt
 
   # Apply configuration by order
+  _notice "Setup SSH"
   # shellcheck disable=SC2086
   chezmoi apply $chezmoi_params $HOME/.ssh
+  _notice "Setup other dotfiles"
   # shellcheck disable=SC2086
   chezmoi apply $chezmoi_params
 
