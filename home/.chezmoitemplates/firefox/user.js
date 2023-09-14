18961a65266822cf04f6b62b274af6ab2e250dde
Trần Đức Nam <dynamo.foss@gmail.com>
2023-09-14 07:35:52 +0700

    feat: add basic settings of firefox

---
+++ home/.chezmoitemplates/firefox/user.js
@@ -0,0 +1,12 @@
+{{/* Include all userscripts in order */}}
+{{- $settings := glob (joinPath .chezmoi.sourceDir ".chezmoitemplates/firefox/prefs/*.js") }}
+{{- if regexMatch ".*personal/user\\.js\\.tmpl" .chezmoi.sourceFile }}
+{{- $settings = concat $settings (glob (joinPath .chezmoi.sourceDir ".chezmoitemplates/firefox/prefs/personal/*.js")) }}
+{{- else if regexMatch ".*enterprise/user\\.js\\.tmpl" .chezmoi.sourceFile }}
+{{- $settings = concat $settings (glob (joinPath .chezmoi.sourceDir ".chezmoitemplates/firefox/prefs/enterprise/*.js")) }}
+{{- else if regexMatch ".*chat/user\\.js\\.tmpl" .chezmoi.sourceFile }}
+{{- $settings = concat $settings (glob (joinPath .chezmoi.sourceDir ".chezmoitemplates/firefox/prefs/chat/*.js")) }}
+{{- end }}
+{{- range ($settings) }}
+{{- output "chezmoi" "execute-template" (include .) }}
+{{- end }}
