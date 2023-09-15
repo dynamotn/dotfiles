f1db9bbd7b1e1ab03a3f1ccc6fd6c27de62cec7d
Trần Đức Nam <dynamo.foss@gmail.com>
2023-09-15 22:07:19 +0700

    feat(firefox): activate lepton

--- home/.chezmoitemplates/firefox/user.js
+++ home/.chezmoitemplates/firefox/user.js
@@ -7,6 +7,7 @@
 {{- else if regexMatch ".*chat/user\\.js\\.tmpl" .chezmoi.sourceFile }}
 {{- $settings = concat $settings (glob (joinPath .chezmoi.sourceDir ".chezmoitemplates/firefox/prefs/chat/*.js")) }}
 {{- end }}
+{{- $settings = concat $settings (list (joinPath .chezmoi.sourceDir ".chezmoitemplates/firefox/chrome/lepton/user.js")) }}
 {{- range ($settings) }}
 {{- output "chezmoi" "execute-template" (include .) }}
 {{- end }}
