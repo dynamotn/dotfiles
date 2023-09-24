3bf72c016c697bc739bb5e2e6b3b554aa14df169
Trần Đức Nam <dynamo.foss@gmail.com>
2023-09-24 23:56:29 +0700

    refactor(dytoy)!: use new method to install tools

---
+++ scripts/os.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+_gentoo_repo_sync() {
+  sudo emerge --sync
+}
+
+_gentoo_install() {
+  if [ ! -x '/usr/bin/qlist' ]; then
+    echo >&2 ":: Error: Must install app-portage/portage-utils first."
+  fi
+  qlist -I "$1" || sudo emerge --ask n --noreplace "$1"
+}
+
+_gentoo_init() {
+  _gentoo_repo_sync
+  _gentoo_install app-portage/gentoolkit
+  _gentoo_install app-portage/portage-utils
+  _gentoo_install app-eselect/eselect-repository
+  sudo eselect repository add dynamo git https://github.com/dynamotn/overlay.git
+}
+
+_arch_repo_sync() {
+  if ! command -v yay > /dev/null; then
+    sudo pacman -Sy
+  else
+    yay -Sy
+  fi
+}
+
+_arch_install() {
+  pacman -Qi "$1" > /dev/null || yay --noconfirm -S --needed --answerclean All --answerdiff None "$1"
+}
+
+_arch_init() {
+  _arch_repo_sync
+  if ! command -v yay > /dev/null; then
+    sudo pacman -S --needed git base-devel
+    git clone https://aur.archlinux.org/yay.git /tmp/yay
+    cd /tmp/yay || exit
+    makepkg -si --noconfirm
+    rm -rf /tmp/yay
+  fi
+}
+
+_ubuntu_repo_sync() {
+  sudo apt update
+}
+
+_ubuntu_install() {
+  dpkg-query -s "$1" 2&>1 || sudo apt install -y "$1"
+}
+
+_ubuntu_init() {
+  _ubuntu_repo_sync
+}
