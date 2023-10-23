2864e858fa9eeaff1ebb3740a57dac42335ea50f
Trần Đức Nam <dynamo.foss@gmail.com>
2023-10-23 10:37:46 +0700

    chore: add gnupg as prerequisite for Ubuntu/Debian

--- prerequisite.sh
+++ prerequisite.sh
@@ -7,7 +7,7 @@ if command -v termux-setup-storage &> /dev/null; then
   pkg update && pkg install -y git curl which
   pkg install -y chezmoi age
 elif command -v apt &> /dev/null; then
-  sudo apt update && sudo apt install -y git curl
+  sudo apt update && sudo apt install -y git curl gnupg
 elif command -v pacman &> /dev/null; then
   sudo pacman -Sy git curl
 elif command -v emerge &> /dev/null; then
