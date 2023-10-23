f1d00f7d41c43bf5a599d6235248e58f4e6d014a
Trần Đức Nam <dynamo.foss@gmail.com>
2023-10-23 15:08:11 +0700

    feat: manage root folder with iptables

--- setup.sh
+++ setup.sh
@@ -57,17 +57,20 @@ _main() {
   _notice "Initialize chezmoi"
   echo "sourceDir: \"$SETUP_DIR\"" > "$SETUP_DIR"/home/.chezmoi.yaml.tmpl
   cat "$SETUP_DIR"/.chezmoi.yaml.tmpl >> "$SETUP_DIR"/home/.chezmoi.yaml.tmpl
-  mkdir -p $HOME/.config/chezmoi/hooks/diff && touch $HOME/.config/chezmoi/hooks/diff/pre.sh
+  mkdir -p "$HOME/.config/chezmoi/hooks/diff" && touch "$HOME/.config/chezmoi/hooks/diff/pre.sh"
   _notice "Please answer the following questions"
   chezmoi init "$chezmoi_params" -S "$SETUP_DIR" --prompt
 
   # Apply configuration by order
   _notice "Setup dytoy"
-  mkdir -p $HOME/.config/dytoy
-  touch $HOME/.config/dytoy/{os,binary}.sh
+  mkdir -p "$HOME/.config/dytoy"
+  touch "$HOME"/.config/dytoy/{os,binary}.sh
   # shellcheck disable=SC2086
   chezmoi apply "$chezmoi_params" $HOME/.config/dytoy
   _install_age
+
+  _notice "Setup operating system"
+  sudo chezmoi --destination / --source "$SETUP_DIR/root" --working-tree "$SETUP_DIR" --config "$HOME/.config/chezmoi/chezmoi.yaml" apply
 
   _notice "Setup SSH"
   # shellcheck disable=SC2086
