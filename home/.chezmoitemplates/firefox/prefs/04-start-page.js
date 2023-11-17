bc09bad0c4e99a00c41ffa9a0e6df3e61198acb4
Trần Đức Nam <dynamo.foss@gmail.com>
2023-11-17 14:24:22 +0700

    fix(firefox): can't open new tab

--- home/.chezmoitemplates/firefox/prefs/04-start-page.js
+++ home/.chezmoitemplates/firefox/prefs/04-start-page.js
@@ -1,4 +1,4 @@
 // Change home page
-user_pref("browser.startup.homepage", "{{ .chezmoi.sourceDir }}/.chezmoitemplates/firefox/startpage/index.html");
+user_pref("browser.startup.homepage", "file://{{ .chezmoi.sourceDir }}/.chezmoitemplates/firefox/startpage/index.html");
 // Change new tab page
-user_pref("browser.newtab.url", "{{ .chezmoi.sourceDir }}/.chezmoitemplates/firefox/startpage/index.html");
+user_pref("browser.newtab.url", "file://{{ .chezmoi.sourceDir }}/.chezmoitemplates/firefox/startpage/index.html");
