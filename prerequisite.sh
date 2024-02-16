a7a9103fd52ac5e816339571b975f2b973eff5c3
Trần Đức Nam <me@dynamotn.dev>
2024-02-16 23:32:06 +0700

    feat: add prerequisite for macos

--- prerequisite.sh
+++ prerequisite.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -Eeuo pipefail
 
-if command -v termux-setup-storage &> /dev/null; then
+if command -v sw_vers &> /dev/null; then
+  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash - || :
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  brew install bash coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep chezmoi age 
+  echo "export PATH=\"/opt/homebrew/opt/coreutils/libexec/gnubin:/opt/homebrew/opt/findutils/libexec/gnubin:/opt/homebrew/opt/gnu-tar/libexec/gnubin:/opt/homebrew/opt/gnu-sed/libexec/gnubin:/opt/homebrew/opt/gawk/libexec/gnubin:/opt/homebrew/opt/gnu-indent/libexec/gnubin:/opt/homebrew/opt/gnu-getopt/bin:/opt/homebrew/opt/grep/libexec/gnubin:$PATH\"" >> ~/.zprofile
+elif command -v termux-setup-storage &> /dev/null; then
   termux-change-repo
   termux-setup-storage
   pkg update && pkg install -y git curl sudo
