6c057a0b108c2f5f17a6ec8f1f143fa2372f7932
Trần Đức Nam <me@dynamotn.dev>
2024-02-18 15:14:28 +0700

    feat(firefox): apply for macos

--- home/.chezmoitemplates/firefox/prefs/enterprise/00-ui.js
+++ home/.chezmoitemplates/firefox/prefs/enterprise/00-ui.js
@@ -1,4 +1,4 @@
-{{- if eq .place "office" }}
+{{- if and (eq .place "office") (eq .kind "pc") }}
 /* Setting the font size of entire UI */
 user_pref("layout.css.devPixelsPerPx", "1.1");
 {{- end }}
