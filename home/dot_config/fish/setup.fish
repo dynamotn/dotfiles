240878348894e09f71b6aee12452efa684e9d378
Trần Đức Nam <me@dynamotn.dev>
2024-12-08 23:48:53 +0700

    Revert "refactor(fish): remove update in setup script"
    This reverts commit d5a04d724bdc50849dc4cdf22d08004200ba4083.

--- home/dot_config/fish/setup.fish
+++ home/dot_config/fish/setup.fish
@@ -1,8 +1,16 @@
 #!/usr/bin/env fish
 set SETUP_DIR (dirname (readlink -m (status --current-filename)))
+set UPDATE false
 
 # Install fisher
-if not functions -q fisher
+for i in (echo $argv | sed "s|--*|\n|g" | grep -v '^$')
+  echo $i | read -l option value
+  switch $option
+    case u update
+      set UPDATE true
+  end
+end
+if not functions -q fisher; or eval $UPDATE
   curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
   and fisher install jorgebucaran/fisher </dev/null
 end
