2993e7d949df9cbe89e2e7d1145d2266ef50c826
Trần Đức Nam <me@dynamotn.dev>
2025-06-12 09:50:48 +0700

    refactor(firefox): use new package for zen on gentoo

--- home/.chezmoitemplates/firefox/prefs/05-zen.js
+++ home/.chezmoitemplates/firefox/prefs/05-zen.js
@@ -8,3 +8,7 @@ user_pref("zen.welcome-screen.seen", true);
 user_pref("zen.theme.accent-color", "#8bd5ca");
 /* Disable new tab button on sidebar */
 user_pref("zen.tabs.show-newtab-vertical", false);
+/* Enable container-specific essentials */
+user_pref("zen.workspaces.container-specific-essentials-enabled", true);
+/* Enable transparency for widget on Linux */
+user_pref("zen.widget.linux.transparency", true);
