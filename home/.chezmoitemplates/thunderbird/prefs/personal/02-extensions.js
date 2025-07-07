bf25c866912b1e47bc861b8e6dc5bda94dda71db
Trần Đức Nam <me@dynamotn.dev>
2025-07-08 01:54:41 +0700

    refactor(thunderbird): change script to sync

--- home/.chezmoitemplates/thunderbird/prefs/personal/02-extensions.js
+++ home/.chezmoitemplates/thunderbird/prefs/personal/02-extensions.js
@@ -1,1 +1,5 @@
-user_pref("extensions.webextensions.uuids", "{\"default-theme@mozilla.org\":\"173a2f1a-73ed-4fb9-ad3d-8a306e4d62d9\"}");
+user_pref("extensions.webextensions.uuids", "{\"default-theme@mozilla.org\":\"173a2f1a-73ed-4fb9-ad3d-8a306e4d62d9\",\"addon@darkreader.org\":\"63955904-55a4-4b1a-9ea2-b7043076bbc7\",\"{d3f3abb5-21aa-57fd-80e3-a9a1887b1d16}\":\"14434d70-f082-4a5e-a8f1-bc98b2f4121c\",\"cardbook@vigneau.philippe\":\"cf9f7a36-e311-46a3-a780-d8ee69337829\",\"uBlock0@raymondhill.net\":\"9299145d-666a-4fa3-a516-8a4db75d8c9b\",\"{2ab1b709-ba03-4361-abf9-c50b964ff75d}\":\"05874deb-4953-454a-a387-ccd856506e57\",\"languagetool-mailextension@languagetool.org\":\"5c98e731-29f8-4451-b6da-81316b68bf0c\"}");
+user_pref("extensions.webextensions.ExtensionStorageIDB.migrated.addon@darkreader.org", true);
+user_pref("extensions.webextensions.ExtensionStorageIDB.migrated.cardbook@vigneau.philippe", true);
+user_pref("extensions.webextensions.ExtensionStorageIDB.migrated.uBlock0@raymondhill.net", true);
+user_pref("extensions.webextensions.ExtensionStorageIDB.migrated.{2ab1b709-ba03-4361-abf9-c50b964ff75d}", true);
