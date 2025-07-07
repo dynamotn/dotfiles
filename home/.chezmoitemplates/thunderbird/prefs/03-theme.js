e6be51ee63148db2cca7e47ddec6996990441302
Trần Đức Nam <me@dynamotn.dev>
2025-07-07 19:57:03 +0700

    feat(thunderbird): add default config of profiles

---
+++ home/.chezmoitemplates/thunderbird/prefs/03-theme.js
@@ -0,0 +1,11 @@
+/* Enable customChrome.css */
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+/* Enable GPU acceleration */
+user_pref("layers.acceleration.force-enabled", true);
+/* Enable WebRender to render page faster */
+user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.enabled", true);
+/* Enable transparent */
+user_pref("layout.css.backdrop-filter.enabled", true);
+/* Allow SVG CSS */
+user_pref("svg.context-properties.content.enabled", true);
