53bd44ac34d9d015e167f6e6f8dcd08a9b51b69e
Trần Đức Nam <me@dynamotn.dev>
2025-07-27 11:01:47 +0700

    feat(firefox): enable ai chat

---
+++ home/.chezmoitemplates/firefox/prefs/06-ai.js
@@ -0,0 +1,11 @@
+/* Enable AI Chat */
+user_pref("browser.ml.chat.enabled", true);
+/* Use localhost providers */
+user_pref("browser.ml.chat.enabled", true);
+/* Default provider */
+user_pref("browser.ml.chat.provider", "https://gemini.google.com");
+/* Enable shortcut for AI chat in right menu context */
+user_pref("browser.ml.chat.shortcuts", true);
+user_pref("browser.ml.chat.shortcuts.custom", true);
+/* Show sidebar of AI chat */
+user_pref("browser.ml.chat.sidebar", true);)
