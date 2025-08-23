e0309d6696a8c494620bfd3f5f83207804340ba0
Trần Đức Nam <me@dynamotn.dev>
2025-08-23 14:36:31 +0700

    fix(chezmoi): check to not start openrc/systemd in container

--- scripts/lib/init_system.sh
+++ scripts/lib/init_system.sh
@@ -10,11 +10,18 @@
 function init::enable_systemd_service {
   local service is_user_service
   dybatpho::expect_args service is_user_service -- "$@"
+  if ! dybatpho::is command systemctl; then
+    dybatpho::warn "systemctl command not found, cannot enable service $service"
+    return
+  fi
   dybatpho::progress "Enabling service $service"
-  if dybatpho::is true "$is_user_service"; then
-    systemctl enable --now --user "$service"
-  else
-    sudo systemctl enable --now "$service"
+
+  if systemctl is-system-running > /dev/null; then
+    if dybatpho::is true "$is_user_service"; then
+      systemctl enable --now --user "$service"
+    else
+      sudo systemctl enable --now "$service"
+    fi
   fi
 }
 
@@ -27,12 +34,19 @@ function init::enable_systemd_service {
 function init::enable_openrc_service {
   local service is_user_service
   dybatpho::expect_args service is_user_service -- "$@"
+  if ! dybatpho::is command rc-service; then
+    dybatpho::warn "rc-service command not found, cannot enable service $service"
+    return
+  fi
   dybatpho::progress "Enabling service $service"
   if dybatpho::is true "$is_user_service"; then
     rc-update --user add "$service"
   else
     sudo rc-update add "$service" default
-    sudo rc-service "$service" start
+    if ! grep -q "docker" /proc/self/cgroup \
+      && ! dybatpho::is file /.dockerenv; then
+      sudo rc-service "$service" start
+    fi
   fi
 }
 
@@ -43,6 +57,10 @@ function init::enable_openrc_service {
 function init::enable_termux_service {
   local service
   dybatpho::expect_args service -- "$@"
+  if ! dybatpho::is command sv; then
+    dybatpho::warn "sv command not found, cannot enable service $service"
+    return
+  fi
   dybatpho::progress "Enabling service $service"
   sv-enable "$service"
   sv up "$service"
@@ -58,6 +76,9 @@ function init::enable_termux_service {
 function init::enable_launchd_service {
   local service is_user_service
   dybatpho::expect_args service is_user_service -- "$@"
+  if ! dybatpho::is command launchctl; then
+    dybatpho::warn "launchctl command not found, cannot enable service $service"
+  fi
   dybatpho::progress "Enabling service $service"
   if dybatpho::is true "$is_user_service"; then
     /usr/bin/osascript -e "tell application \"System Events\" to make new login item at end with properties {name:\"${service}.app\", path:\"/Applications/${service}.app\", kind:\"Application\", hidden:true}"
