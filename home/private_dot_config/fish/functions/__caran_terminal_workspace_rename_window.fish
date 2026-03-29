974730ed24c59688a621eefbaa95d7ac49c6bc77
Trần Đức Nam <me@dynamotn.dev>
2026-03-29 15:29:33 +0700

    feat(ssh): rename kitty window

--- home/private_dot_config/fish/functions/__caran_terminal_workspace_rename_window.fish
+++ home/private_dot_config/fish/functions/__caran_terminal_workspace_rename_window.fish
@@ -4,5 +4,7 @@ function __caran_terminal_workspace_rename_window -d "Rename terminal workspace 
     else if test -n "$TMUX"
         set -g OLD_TMUX_WINDOW_NAME (tmux list-windows -F '#{window_name}#{window_active}' | sed -n 's|^\(.*\)1$|\1|p')
         tmux rename-window $name
+    else if test -n "$KITTY_WINDOW_ID"
+        kitten @ --to unix:///tmp/kitty-$KITTY_PID set-tab-title --match id:$KITTY_WINDOW_ID $name
     end
 end
--- home/private_dot_config/fish/functions/__caran_terminal_workspace_undo_rename_window.fish
+++ home/private_dot_config/fish/functions/__caran_terminal_workspace_undo_rename_window.fish
@@ -3,5 +3,7 @@ function __caran_terminal_workspace_undo_rename_window -d "Recover terminal work
         zellij action undo-rename-tab
     else if test -n "$TMUX"
         tmux rename-window $OLD_TMUX_WINDOW_NAME
+    else if test -n "$KITTY_WINDOW_ID"
+        kitten @ --to unix:///tmp/kitty-$KITTY_PID set-tab-title --match id:$KITTY_WINDOW_ID ""
     end
 end
